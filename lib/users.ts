39b8ef1dd4bd8b320275dad3e40a792febf85b21
mohamedsa <mohamedsa68599@outlook.com>
2026-03-15 10:57:28 +0200

    fixd env

--- lib/users.ts
+++ lib/users.ts
@@ -5,7 +5,7 @@ const authConfig: sql.config = {
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
   server: process.env.DB_SERVER || "localhost",
-  database: "system_DB",
+  database: process.env.AUTH_DATABASE!,
   port: parseInt(process.env.DB_PORT || "1433"),
   options: {
     encrypt: process.env.DB_ENCRYPT === "true",
